340f79ede960204f1cca5ca31fa4b550948989d4
qurtubi <qurtubi1411@gmail.com>
2026-01-28 20:21:28 +0700

    update: layout navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,9 @@
-export default function Navbar(){
-    
-    return(
-        <div className="flex flex-row-reverse border-b border-slate-300 py-3 px-8">
-            <button className="bg-blue-600 px-6 py-2 rounded-lg text-white">+ Tambah Transaksi</button>
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <div className="flex flex-row-reverse border-b border-slate-300 py-3 px-8">
+      <button className="bg-indigo-600 px-6 py-2 rounded-lg text-white hover:cursor-pointer hover:bg-indigo-600/70 transition-all duration-300">
+        + Tambah Transaksi
+      </button>
+    </div>
+  );
 }
