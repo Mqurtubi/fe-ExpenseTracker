47c840e1714c1460a568a57875ac670e5c38995e
qurtubi <qurtubi1411@gmail.com>
2026-01-30 18:14:25 +0700

    add: http and interceptors

---
+++ src/services/interceptors.ts
@@ -0,0 +1,32 @@
+import type {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { http } from "./http";
+
+http.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+http.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response.data.data;
+  },
+  (error: AxiosError) => {
+    const status = error.response?.status;
+    if (status === 401) {
+      window.location.href = "/login";
+    }
+    if (status === 403) {
+      console.error("forbidden");
+    }
+    if (status && status >= 500) {
+      console.error("internal server error");
+    }
+    return Promise.reject(error);
+  },
+);
