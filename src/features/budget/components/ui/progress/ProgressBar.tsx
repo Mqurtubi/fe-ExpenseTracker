ba13e4d1e32f2e5803aca7b4b2d66226775e0419
qurtubi <qurtubi1411@gmail.com>
2026-02-21 06:50:36 +0700

    update: width progress bar

--- src/features/budget/components/ui/progress/ProgressBar.tsx
+++ src/features/budget/components/ui/progress/ProgressBar.tsx
@@ -2,7 +2,10 @@ export default function ProgressBar({ progress }: { progress: number }) {
   const value = Math.max(0, Math.min(100, progress));
   return (
     <div className="w-full h-3 bg-slate-200 rounded-2xl overflow-hidden">
-      <div className={`bg-green-600 w-[${value}%] h-3`}></div>
+      <div
+        className={`bg-green-600 h-3 transition-[width] duration-300`}
+        style={{ width: `${value}%` }}
+      ></div>
     </div>
   );
 }
